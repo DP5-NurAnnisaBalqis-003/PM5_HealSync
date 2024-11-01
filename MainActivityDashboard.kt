b6ee65e13a3faab1266d27bd73c7c7fd71c7b418
Winda Rinjani Adhana <114667475+DP5-WindaRinjaniAdhana-009@users.noreply.github.com>
2024-11-01 13:43:54 +0700

    Update MainActivityDashboard.kt

--- MainActivityDashboard.kt
+++ MainActivityDashboard.kt
@@ -18,12 +18,12 @@ class MainActivityDashboard : AppCompatActivity() {
         setContentView(binding.root)
 
         val recipes = listOf(
-            Recipe("Jahe", "Kue jahe yang manis, dibuat dengan jahe, tepung, gula, dan madu.", R.drawable.jahe_image),
-            Recipe("Onde-onde Ketawa", "Kue khas Sumatra berbentuk bulat dengan biji wijen.", R.drawable.ondeonde_image),
-            Recipe("Cokelat Mete", "Kue kering cokelat dengan mete sebagai topping.", R.drawable.cokelat_image),
-            Recipe("Almond Crispy", "Kue tipis dan renyah dengan irisan almond.", R.drawable.almond_image),
-            Recipe("Sagu Pandan Keju", "Kue sagu pandan yang dipadu dengan keju.", R.drawable.sagu_image),
-            Recipe("Kacang", "Kue kering kacang dengan rasa manis-gurih.", R.drawable.kacang_image)
+            Recipe("Jahe", "Kue jahe mengacu pada kategori luas makanan yang dipanggang, biasanya dibumbui dengan jahe, cengkeh, pala, dan kayu manis, yang diberi pemanis seperti madu, gula, atau molase.", R.drawable.jahe_image),
+            Recipe("Onde-onde Ketawa", "Kue ketawa adalah camilan khas Kota Pematangsiantar, Sumatera Utara yang berbentuk bulat dengan belahan di tengahnya sehingga terlihat seperti tersenyum atau tertawa.", R.drawable.ondeonde_image),
+            Recipe("Cokelat Mete", "Kue kering rasa cokelat diberi topping mete di atas nya.", R.drawable.cokelat_image),
+            Recipe("Almond Crispy", "Kue almond crispy adalah sejenis kue tipis dan renyah yang biasanya terbuat dari bahan utama seperti tepung, mentega, gula, putih telur, dan irisan almond.", R.drawable.almond_image),
+            Recipe("Sagu Pandan Keju", "Sagu pandan keju adalah kue kering yang terbuat dari tepung sagu dengan rasa pandan dan taburan keju.", R.drawable.sagu_image),
+            Recipe("Kacang", "Kue kacang adalah jenis kue kering yang berbahan dasar kacang tanah, tepung, gula, dan minyak atau margarin. Kue ini memiliki tekstur yang renyah dan rasa yang gurih-manis dengan aroma kacang yang khas.", R.drawable.kacang_image)
         )
 
         recipeAdapter = RecipeAdapter(recipes)
