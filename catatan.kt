b3bfaea08699dbe18f2be708c0d6a9378eb4a9e6
DP5-deviaanjani-019 <114506379+DP5-deviaanjani-019@users.noreply.github.com>
2024-11-04 21:21:57 +0700

    Create catatan.kt

---
+++ catatan.kt
@@ -0,0 +1,33 @@
+package com.lab5.myapplication
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class catatan : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_catatan)
+
+        // Setup padding to avoid overlaps with system bars
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val addButton: Button = findViewById(R.id.btn_tambah)
+
+        addButton.setOnClickListener {
+            Toast.makeText(this, "Catatan ditambahkan!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
