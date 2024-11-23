12930cc632405e7142d0758f1fd47269ccc4a2e5
DP5-deviaanjani-019 <114506379+DP5-deviaanjani-019@users.noreply.github.com>
2024-11-24 00:52:35 +0700

    Update LoginActivity.kt

--- LoginActivity.kt
+++ LoginActivity.kt
@@ -4,8 +4,10 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,7 +16,7 @@ class LoginActivity : AppCompatActivity() {
 
     private val sharedPrefFile = "com.example.cakemate.PREFERENCE_FILE_KEY"
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mylogin)
@@ -22,11 +24,27 @@ class LoginActivity : AppCompatActivity() {
         val loginButton = findViewById<Button>(R.id.btn_login)
         val usernameInput = findViewById<EditText>(R.id.namapengguna)
         val passwordInput = findViewById<EditText>(R.id.password)
-        val registerTextView = findViewById<TextView>(R.id.registerTextView) // Tambahan untuk navigasi ke registrasi
+        val togglePasswordView = findViewById<ImageView>(R.id.iv_toggle_confirm_password)
+        val registerTextView = findViewById<TextView>(R.id.registerTextView)
 
         val sharedPreferences = getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
         val savedUsername = sharedPreferences.getString("REGISTERED_USERNAME", null)
         val savedPassword = sharedPreferences.getString("REGISTERED_PASSWORD", null)
+
+        var isPasswordVisible = false
+
+        togglePasswordView.setOnClickListener {
+            isPasswordVisible = !isPasswordVisible
+            if (isPasswordVisible) {
+                passwordInput.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                togglePasswordView.setImageResource(R.drawable.eyehidden) // Ganti dengan ikon mata terbuka
+            } else {
+                passwordInput.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                togglePasswordView.setImageResource(R.drawable.eyepw) // Ganti dengan ikon mata tertutup
+            }
+            // Arahkan kursor ke akhir teks
+            passwordInput.setSelection(passwordInput.text.length)
+        }
 
         loginButton.setOnClickListener {
             val enteredUsername = usernameInput.text.toString()
