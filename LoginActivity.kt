5f72506e1b08c37eca4af38ae802c88b5225482a
DP5-deviaanjani-019 <114506379+DP5-deviaanjani-019@users.noreply.github.com>
2024-11-23 23:43:48 +0700

    Update LoginActivity.kt

--- LoginActivity.kt
+++ LoginActivity.kt
@@ -1,11 +1,10 @@
 package com.example.cakemate
 
-import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,7 +12,6 @@ class LoginActivity : AppCompatActivity() {
 
     private val sharedPrefFile = "com.example.cakemate.PREFERENCE_FILE_KEY"
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mylogin)
@@ -21,8 +19,9 @@ class LoginActivity : AppCompatActivity() {
         val loginButton = findViewById<Button>(R.id.btn_login)
         val usernameInput = findViewById<EditText>(R.id.namapengguna)
         val passwordInput = findViewById<EditText>(R.id.password)
+        val registerTextView = findViewById<TextView>(R.id.tv_register) // Tambahkan ini
 
-        val sharedPreferences = getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+        val sharedPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE)
         val savedUsername = sharedPreferences.getString("REGISTERED_USERNAME", null)
         val savedPassword = sharedPreferences.getString("REGISTERED_PASSWORD", null)
 
@@ -32,13 +31,17 @@ class LoginActivity : AppCompatActivity() {
 
             if (enteredUsername == savedUsername && enteredPassword == savedPassword) {
                 Toast.makeText(this, "Masuk berhasil", Toast.LENGTH_SHORT).show()
-
                 val intent = Intent(this, Dashboard::class.java)
                 startActivity(intent)
                 finish()
             } else {
                 Toast.makeText(this, "Nama pengguna atau kata sandi salah", Toast.LENGTH_SHORT).show()
             }
+        }
+
+        registerTextView.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
