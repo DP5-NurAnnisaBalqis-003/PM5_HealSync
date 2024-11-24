4ad9b9e3e642554c0569095899143e6bf38612c3
DP5-deviaanjani-019 <114506379+DP5-deviaanjani-019@users.noreply.github.com>
2024-11-24 19:20:35 +0700

    Update Upload1.kt

--- Upload1.kt
+++ Upload1.kt
@@ -1,4 +1,4 @@
-package com.lab5.myapplication
+package com.example.cakemate
 
 import android.annotation.SuppressLint
 import android.content.Intent
@@ -7,6 +7,7 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Upload1 : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -20,12 +21,34 @@ class Upload1 : AppCompatActivity() {
             insets
         }
 
-         val btnSelanjutnya = findViewById<Button>(R.id.btnSelanjutnya)
-
+        val btnSelanjutnya = findViewById<Button>(R.id.btnSelanjutnya)
         btnSelanjutnya.setOnClickListener {
             val intent = Intent(this, Upload2::class.java)
             startActivity(intent)
         }
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNav.setOnItemSelectedListener { menuItem ->
+            if (bottomNav.selectedItemId != menuItem.itemId) { // Hanya navigasi jika item berbeda
+                when (menuItem.itemId) {
+                    R.id.nav_dashboard -> {
+                        startActivity(Intent(this, Dashboard::class.java))
+                        finish() // Tutup aktivitas saat ini untuk mencegah tumpukan aktivitas
+                    }
+                    R.id.nav_menu -> {
+                        startActivity(Intent(this, MainActivityDashboard::class.java))
+                        finish()
+                    }
+                    R.id.nav_save_recipe -> {
+                        startActivity(Intent(this, SimpanResep::class.java))
+                        finish()
+                    }
+                }
+            }
+            true
+        }
+
+        bottomNav.selectedItemId = R.id.nav_upload
     }
 }
-
