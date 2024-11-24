45cbbbc29ebe49f24500d29f695410dbd0993fad
Winda Rinjani Adhana <114667475+DP5-WindaRinjaniAdhana-009@users.noreply.github.com>
2024-11-24 19:44:00 +0700

    ResepAlmond,Jahe,Mete,Onde,Sagu.kt

---
+++ ResepMete.kt
@@ -0,0 +1,12 @@
+package com.example.projectuas
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResepMete : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resep_mete)
+    }
+}
